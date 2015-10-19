82c01b6eeadf6c67bc0f8603924ef5d24aa3f529
Terence-Lee Davis <zinglish@gmail.com>
2015-10-20 00:16:06 +0100

    Fixed a few things and removed the logs

--- Content/Defragr/UI/js/mainmenu.js
+++ Content/Defragr/UI/js/mainmenu.js
@@ -18,7 +18,10 @@ _MainMenu.prototype = {
 	{
 		UE4.getFileList(UE4.getMapsDir(), function(files)
 		{
-			
+			for(i = 0; i < files.length; i++)
+			{
+				$('.map-list ul').append("<li>" + files[i] + "</li>");
+			}
 		});
 	}
 }
@@ -26,15 +29,15 @@ var MainMenu = new _MainMenu();
 
 
 // Initialize the virtual console
-window.onload = function()
-{
+Zepto(function($){
+
 	mobileConsole.options({
 		showOnError: true,
 		proxyConsole: true,
 		isCollapsed: true,
 		catchErrors: true
 	});
-}
+});
 
 // Initialize the main menu application
 UE4.ready(function(){ MainMenu._init(); });
